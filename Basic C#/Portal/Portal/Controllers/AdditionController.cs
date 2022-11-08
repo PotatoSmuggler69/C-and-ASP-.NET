77740ec62319108ab8f5b0b1f05643a556ec9121
Abhishek Manna <86219034+PotatoSmuggler69@users.noreply.github.com>
2022-11-08 22:14:20 +0530

    First MVC Project added of adding two numbers

---
+++ Basic C#/Portal/Portal/Controllers/AdditionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portal.Models;
+namespace Portal.Controllers
+{
+    public class AdditionController : Controller
+    {
+        public IActionResult GetValue()
+        {
+
+            return View();
+        }
+        public IActionResult SetValue(int x, int y)
+        {
+            Addition obj = new Addition();
+            obj.x=x;
+            obj.y=y;
+            obj.sum = x + y;
+
+            return View(obj);
+        }
+    }
+}
---
+++ Basic C#/Portal/Portal/Models/Addition.cs
@@ -0,0 +1,9 @@
+﻿namespace Portal.Models
+{
+    public class Addition
+    {
+        public int x { set; get; }
+        public int y { get; set; }
+        public int sum{get; set;}
+    }
+}
